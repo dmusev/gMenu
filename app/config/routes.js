294c25c3abcbe6211243fd1c7cca7110cd6e4444
dmusev <musevche@gmail.com>
2017-02-10 16:33:48 +0200

    Routing bug fix + pug removal

--- app/config/routes.js
+++ app/config/routes.js
@@ -8,21 +8,20 @@ mongoose.Promise = global.Promise
 let controllers = require('./../controllers/index')
 
 module.exports = (app) => {
-  // server routes/authentication/api calls
-  app.get('/api/users', controllers.users.getUsers)
+    // server routes/authentication/api calls
+    app.get('/api/users', controllers.users.getUsers)
 
-  app.post('/api/users/authenticate', controllers.users.authenticate)
+    app.post('/api/users/authenticate', controllers.users.authenticate)
 
-  app.post('/api/users/create', controllers.users.create)
-  app.post('/api/users/logout', controllers.users.logout)
+    app.post('/api/users/create', controllers.users.create)
+    app.post('/api/users/logout', controllers.users.logout)
 
-  // TODO: implement global controllers function handler
-  // app.all('api/:controller/:method/:id', (req, res) => {
-  //   controllers[req.params.controller].req.params.method(id)
-  // })
+    // TODO: implement global controllers function handler
+    // app.all('api/:controller/:method/:id', (req, res) => {
+    //   controllers[req.params.controller].req.params.method(id)
+    // })
 
-  app.get('/', controllers.home.index)
+    app.get('/', controllers.home.index)
 
-  app.all('*', controllers.home.index)
+    app.get('*', controllers.home.index)
 }
-
--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -1,8 +1,10 @@
 /**
  * Created by D.Musev on 31.10.2016 г..
  */
+var path = require('path');
+
 module.exports = {
     index: (req, res) => {
-        res.sendfile('./../../public/index.html')
+        res.sendFile('index.html', { root: path.join(__dirname, '../../public') });
     }
 }
